c587f960f9a58ca1101f6c41589e0616dc0b39c8
Gregor Harlan <gharlan@web.de>
2012-10-29 23:51:22 +0100

    init

---
+++ config.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+// Diese Datei ist keine Pflichdatei mehr.
+
+// Klassen und lang-Dateien müssen hier nicht mehr eingebunden werden, sie werden nun automatisch gefunden.
+
+// Daten wie Autor, Version, Subpages etc. sollten wenn möglich in der package.yml notiert werden.
+// Sie können aber auch weiterhin hier gesetzt werden:
+$this->setProperty('author', 'Gregor Harlan');
